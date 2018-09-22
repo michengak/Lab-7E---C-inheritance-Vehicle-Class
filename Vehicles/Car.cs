117822f14f6513cab03c62842f3fcae5da7312ec
michel ngako <emoumichel@gmail.com>
2018-09-22 00:08:16 -0400

    Add project files.

---
+++ Vehicles/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    class Car: Vehicle
+    {
+        public void Accelerate()
+        {
+            Console.WriteLine("Accelerating");
+        }
+
+        public void Brake()
+        {
+            Console.WriteLine("Braking");
+        }
+    }
+}
